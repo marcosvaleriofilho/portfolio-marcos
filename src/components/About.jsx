d5667a2dee31ebd82316a9e2be82a697c990e335
marcosvaleriofilho <marcosvaleriofilho@outlook.com>
2024-09-06 16:34:49 -0300

    feat: alterações CV e sobre

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,12 +11,7 @@ const About = () => {
         <motion.div  whileInView={{opacity: 1, y: 0,}} initial={{opacity: 0, y: 100}} transition={{duration: 1, delay: 1}} className='w-full'>
           <div className='flex items-center justify-center'>
             <p className='max-w-6xl text-center my-10 mb-20'>
-              Estudante de Engenharia de Software, atualmente cursando 6º semestre na PUC-Campinas.
-              Atuando como bolsista pesquisador em Análise de Dados na Embrapa Agricultura Digital.
-              Possuo um forte interesse em desenvolvimento web e mobile, Business Intelligence, Ciência de Dados e Inteligência Artificial,
-              com foco em Machine Learning. Sou criativo, comunicativo, flexível e resiliente, sempre em busca de novas tecnologias e conhecimentos.
-              Procuro oportunidades em empresas de tecnologia em crescimento, onde possa aplicar e expandir minhas habilidades,
-              desenvolvendo-me em um ambiente que ofereça um plano de carreira sólido.
+            Estudante de Engenharia de Software no 6º semestre na PUC-Campinas com experiência prática em análise de dados e desenvolvimento de software. Atuo como bolsista pesquisador na Embrapa Agricultura Digital e tenho forte interesse em desenvolvimento web, mobile e Machine Learning. Busco uma posição em empresas de tecnologia em crescimento para aplicar e expandir minhas habilidades, com foco em soluções inovadoras e desenvolvimento contínuo.
             </p>
           </div>
         </motion.div>
